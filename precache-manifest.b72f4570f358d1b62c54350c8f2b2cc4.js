c70c9e22d71b8bbac3be4030bd83dbe5e723d67e
JanetZheng516 <janet@pop-os.localdomain>
2020-12-07 17:57:09 +0100

    Updates

---
+++ precache-manifest.b72f4570f358d1b62c54350c8f2b2cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b610843c3dc034cee41655cb8f67ee",
+    "url": "/kitchen_planner/index.html"
+  },
+  {
+    "revision": "3dfc2c7b4de5cf203993",
+    "url": "/kitchen_planner/static/css/main.5a74d603.chunk.css"
+  },
+  {
+    "revision": "84aa62e4cdac3a77226e",
+    "url": "/kitchen_planner/static/js/2.8949381e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kitchen_planner/static/js/2.8949381e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dfc2c7b4de5cf203993",
+    "url": "/kitchen_planner/static/js/main.77745485.chunk.js"
+  },
+  {
+    "revision": "864844de61d0f4ff3a5a",
+    "url": "/kitchen_planner/static/js/runtime-main.be63be19.js"
+  }
+]);
